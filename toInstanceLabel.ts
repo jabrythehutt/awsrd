722a6e5158a352cddc2d99ec95cc1eadf4148649
Daniel Jabry <daniel.jabry@rezonence.com>
2023-04-02 12:02:24 +0100

    Format sources

--- toInstanceLabel.ts
+++ toInstanceLabel.ts
@@ -1,9 +1,7 @@
-import {
-    Instance,
-  } from "@aws-sdk/client-ec2";
+import { Instance } from "@aws-sdk/client-ec2";
 
 export function toInstanceLabel(instance: Instance): string {
-    const name = instance.Tags?.find((t) => t.Key === "Name")?.Value;
-    const id = instance.InstanceId as string;
-    return name ? `${name} (${id})` : id;
+  const name = instance.Tags?.find((t) => t.Key === "Name")?.Value;
+  const id = instance.InstanceId as string;
+  return name ? `${name} (${id})` : id;
 }
